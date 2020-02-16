563d21dd0e492aabb8a3c05b1e7662910bf46bc1
IndongLee <kundera@kakao.com>
2020-02-17 01:23:26 +0900

    Learn SQLite in Android / Create Team Manage

---
+++ 15_SQLite_ManageTeamMember/app/src/main/java/com/example/a15_sqlite_manageteammember/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.a15_sqlite_manageteammember;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    myDBHelper myHelper;
+    EditText edtName, edtNumber, edtNameResult, edtNumberResult;
+    Button btnInit, btnInsert, btnSelect;
+    SQLiteDatabase sqlDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("팀 관리 DB");
+
+        edtName = (EditText) findViewById(R.id.edtName);
+        edtNumber = (EditText) findViewById(R.id.edtNumber);
+        edtNameResult = (EditText) findViewById(R.id.edtNameResult);
+        edtNumberResult = (EditText) findViewById(R.id.edtNumberResult);
+        btnInit = (Button) findViewById(R.id.btnInit);
+        btnInsert = (Button) findViewById(R.id.btnInsert);
+        btnSelect = (Button) findViewById(R.id.btnSelect);
+
+        myHelper = new myDBHelper(this);
+        btnInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myHelper.getWritableDatabase();
+                myHelper.onUpgrade(sqlDB, 1, 2);
+                sqlDB.close();
+            }
+        });
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myHelper.getWritableDatabase();
+                sqlDB.execSQL("INSERT INTO teamTBL VALUES ('"
+                        + edtName.getText().toString() + "' , "
+                        + edtNumber.getText().toString() + ");");
+                sqlDB.close();
+                Toast.makeText(getApplicationContext(), "입력되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myHelper.getReadableDatabase();
+                Cursor cursor;
+                cursor = sqlDB.rawQuery("SELECT * FROM teamTBL;", null);
+
+                String strNames = "팀 이름" + "\r\n" + "_________" + "\r\n";
+                String strNumbers = "인원" + "\r\n" + "_________" + "\r\n";
+
+                while (cursor.moveToNext()) {
+                    strNames += cursor.getString(0) + "\r\n";
+                    strNumbers += cursor.getString(1) + "\r\n";
+                }
+
+                edtNameResult.setText(strNames);
+                edtNameResult.setText(strNumbers);
+
+                cursor.close();
+                sqlDB.close();
+            }
+        });
+    }
+
+    public class myDBHelper extends SQLiteOpenHelper {
+        public myDBHelper(Context context) {
+            super(context, "teamDB", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE teamTBL (tname CHAR(20) PRIMARY KEY, tNumber INTEGER);");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS teamTBL");
+            onCreate(db);
+        }
+    }
+}
