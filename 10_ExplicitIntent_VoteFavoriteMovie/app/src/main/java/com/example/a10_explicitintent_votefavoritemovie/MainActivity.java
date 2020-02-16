79a8130c3952020eba33d4ebe4f1ebb63cf91007
IndongLee <asghost@naver.com>
2020-02-16 20:23:28 +0900

    Learn Explicit Intent / Create Simple Vote Favorite Movie App

---
+++ 10_ExplicitIntent_VoteFavoriteMovie/app/src/main/java/com/example/a10_explicitintent_votefavoritemovie/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.a10_explicitintent_votefavoritemovie;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("영화 선호도 투표");
+
+        final int voteCount[] = new int[9];
+        for (int i = 0; i < 9; i++) {
+            voteCount[i] = 0;
+        }
+
+        ImageView image[] = new ImageView[9];
+        Integer imageId[] = {R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5, R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9};
+
+        for (int i = 0; i < imageId.length; i++) {
+            final int index;
+            index = i;
+            image[index] = (ImageView) findViewById(imageId[index]);
+            image[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    voteCount[index]++;
+                    Toast.makeText(getApplicationContext(), "총 " + voteCount[index] + " 표", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        Button btnResult = (Button) findViewById(R.id.btnResult);
+        btnResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                intent.putExtra("VoteCount", voteCount);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ 10_ExplicitIntent_VoteFavoriteMovie/app/src/main/java/com/example/a10_explicitintent_votefavoritemovie/ResultActivity.java
@@ -0,0 +1,43 @@
+package com.example.a10_explicitintent_votefavoritemovie;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+        setTitle("투표 결과");
+
+        Intent intent = getIntent();
+        int[] voteResult = intent.getIntArrayExtra("VoteCount");
+
+        TextView[] tv = new TextView[voteResult.length];
+        RatingBar[] rbar = new RatingBar[voteResult.length];
+
+        Integer[] tvID = {R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5, R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9};
+        Integer[] rbarID = {R.id.rbar1, R.id.rbar2, R.id.rbar3, R.id.rbar4, R.id.rbar5, R.id.rbar6, R.id.rbar7, R.id.rbar8, R.id.rbar9};
+
+        for (int i = 0; i < voteResult.length; i++) {
+            tv[i] = (TextView) findViewById(tvID[i]);
+            rbar[i] = (RatingBar) findViewById(rbarID[i]);
+            rbar[i].setRating((float) voteResult[i]);
+        }
+
+        Button btnReturn = (Button) findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
